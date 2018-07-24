361ff52bc3cca575f81fc8b50065d8170c847e09
user <>
2018-07-24 21:09:39 +0900

    mig

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,8 @@
+var unlock = require("../unlock");
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer,network) {
   console.log(network);
+  unlock(network);
   deployer.deploy(Migrations);
 };
---
+++ readme.txt
@@ -0,0 +1,6 @@
+truffle init
+npm init -y
+
+modify unlock.js
+
+ACCOUNT_PASSWORD=passwd truffle migrate --network danet
